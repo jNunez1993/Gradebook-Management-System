2b5925fd9eff19ee669063e3578264a6f70d77dd
David <dyeung93@hotmail.com>
2014-12-06 14:07:47 -0500

    added functionality for prof course sidemenu

--- includes/profHomeCourse.php
+++ includes/profHomeCourse.php
@@ -3,18 +3,24 @@ include ('functions.php');
 	$ufid = $_SESSION["UFID"];
 	$conn= connect();
 	
-	$query = "SELECT DISTINCT course.course_name, professor.fname, professor.lname 
+	$query = "SELECT DISTINCT course.course_name 
 	FROM course, professor WHERE course.professor_ID = '$ufid' and course.professor_ID = professor.UFID"; 
 	
 	$stid = oci_parse($conn,$query);
 	oci_execute($stid);
 
-	echo ' <table class = "table table-hover table-condensed">';
+	echo ' 
+	<h4 class="list-group-item-heading">Courses</h4>
+	<table class = "table table-hover table-stripped">';
 		echo '	<th> Course Name </th> 
-				<th> Professor Name </th>';
+				<th> View Main Page </th>';
+
 	while (($row=oci_fetch_row($stid))!=false){
 			echo '<tr>';
 			echo '<td>' . $row[0] . '</td>';
+			echo '<td> 
+			<a href = "profCourse.php?course='. $row[0] .' " class = "btn btn-warning" id = "' . $row[0] . '">
+			View Course Main Page </a> </td>';
 			echo '</tr>';  
 	}
 	echo '</table>';
