233c62cb7856372eefe4c299c4d3b592c18703f9
victorh3 <victorh3@live.com>
2014-12-06 00:51:11 -0500

    Fixed bug on the average table
    Doesn’t print the extra box from Tom’s work.

--- includes/changeCourseGrade.php
+++ includes/changeCourseGrade.php
@@ -1,43 +1,51 @@
 <?php
+
 include ('functions.php');
+
+if(isset($_POST['type'])){
+	//Place header for actual queries stuff
 	$ufid = $_SESSION["UFID"];
 	$course = $_SESSION["Course"];
 	$conn= connect();
 	
 
-	$query = 
-			"SELECT distinct assignment_name,grade
-			FROM grade WHERE grade.student_ID = '$ufid' 
-			AND grade.course_name = '$course'";
+	if ($_POST['type'] == "Grades") {
+		$query = "SELECT distinct assignment_name, grade 
+		FROM grade,course 
+		WHERE grade.student_ID = '$ufid' 
+		AND grade.course_name = '$course' 
+		ORDER BY assignment_name ASC "; 
+//NEEDS TO BE GENERIC
 
-	$stid = oci_parse($conn,$query);
-	oci_execute($stid);
+		$stid = oci_parse($conn,$query);
+		oci_execute($stid);
 
-	echo ' <table class = "table table-hover table-condensed">';
-	echo '	<tr>
-				<th>Assignment </th>
-				<th>Your Grade </th>
-				<th>Class Average </th> 
-			</tr>
+		echo '<table class = "table table-hover table-condensed">';
+		echo '	
+					<th> Assignment Name </th>
+					<th> Assignment Grade </th>
+					<th> Class Average </th>
 			';
-
-	while (true == ($row = oci_fetch_row($stid))) {
-			echo '<tr>';
-			echo '<td>' . $row[0] . '</td>';
-			echo '<td>' . $row[1]. '</td>';
-		foreach(array_unique($row) as $item){
-			$query_average = 	
-								"SELECT distinct TRUNC(AVG(grade),1) 
-								AS average FROM grade where grade.course_name = '$course' 
+		$counter = 1;
+		while (($row = oci_fetch_row($stid)) != false) {
+			echo 	'<tr>
+						<td> ' . $row[0]  . '</td> 
+						<td> ' . $row[1]  . '</td>';
+			foreach ($row as $item) {
+			$query_average = "	SELECT distinct TRUNC(AVG(grade),2) 
+								AS average FROM grade 
+								where grade.course_name = '$course' 
 								AND grade.assignment_name = '$item' ";
-
 			$avgID = oci_parse($conn,$query_average);
 			oci_execute($avgID);
 			$average = oci_fetch_row($avgID);
-			echo '<td>' . $average[0] . '</td>';
-			echo '</tr>';  
-			//oci_result($avgID, 'average')
-		}
+			echo '<td>'. $average[0] . '</td>';
+			$counter++;
+			}
+			echo '</tr>';
 	}
-	echo '</table>';
+		echo '</table>';
+
+	}
+}
 ?>
