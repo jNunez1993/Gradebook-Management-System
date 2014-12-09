075cfde7b04a6afbe74ea13b01cf590857d934da
victorh3 <victorh3@live.com>
2014-12-09 16:00:36 -0500

    Updated the number of tuples button
    Wrote queries to calculate the total amount of tuples in all our
    databases.

--- index.php
+++ index.php
@@ -32,11 +32,36 @@
 	
 	<?php 
 	$conn=connect();
-	$query= "select sum(num_rows) from (select table_name, num_rows from user_tables)";
-	$stid=oci_parse($conn,$query);
-	oci_execute($stid);
-	$row=oci_fetch_array($stid);
-	$number = $row[0];
+	$query1= "SELECT COUNT(id) FROM announcements";
+	$stid1=oci_parse($conn,$query1);
+	oci_execute($stid1);
+	$row1=oci_fetch_array($stid1);
+	$number1 = $row1[0];
+	$conn=connect();
+	$query2= "SELECT COUNT(grade) FROM grade";
+	$stid2=oci_parse($conn,$query2);
+	oci_execute($stid2);
+	$row2=oci_fetch_array($stid2);
+	$number2 = $row2[0];
+	$conn=connect();
+	$query3= "SELECT COUNT(ufid) FROM professor";
+	$stid3=oci_parse($conn,$query3);
+	oci_execute($stid3);
+	$row3=oci_fetch_array($stid3);
+	$number3 = $row3[0];
+	$conn=connect();
+	$query4= "SELECT COUNT(ufid) FROM student";
+	$stid4=oci_parse($conn,$query4);
+	oci_execute($stid4);
+	$row4=oci_fetch_array($stid4);
+	$number4 = $row4[0];
+	$conn=connect();
+	$query5= "SELECT COUNT(course_id) FROM course";
+	$stid5=oci_parse($conn,$query5);
+	oci_execute($stid5);
+	$row5=oci_fetch_array($stid5);
+	$number5 = $row5[0];
+	$number = $number1 + $number2 + $number3 + $number4 + $number5
 	?>
 	
 	<form action="index.php" method="post">
